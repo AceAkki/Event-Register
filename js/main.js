810b5df455ba48ea3c6830c26d72e50580d020ea
Akshay R (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2025-10-20 17:25:41 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
   $("#header").load("header.html", function () {
     let headerHeight = document.querySelector(".container-fluid").getBoundingClientRect().height + 20;
     document.documentElement.style.setProperty("--header-height", `${Math.round(headerHeight)}px`);
+
+    window.addEventListener("scroll", ()=>{
+      if (scrollY > 20) {
+        this.querySelector(".navbar").classList.add("glass-dark");
+      } else {
+        this.querySelector(".navbar").classList.remove("glass-dark");        
+      }
+    })
   });
 
   AOS.init();
